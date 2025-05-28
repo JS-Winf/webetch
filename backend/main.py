a22387625c110e416f0fa5e509abea8564674db8
Jay <allmail@posteo.de>
2025-05-28 12:46:20 +0200

    Grundgerüst Flask drin in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,28 @@
 #Einstiegspunkt
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
+
+app = Flask(__name__)
+app.secret_key = os.random(24) # Sessions für zb. Login
+
+@app.route("/")
+def index():
+    return render_template("index.html") #Startseite
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #Hier später Login Logik
+        return redirect(url_for("index"))
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def regiser():
+    if request.method == "POST":
+        # Hier später Registrierung
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
