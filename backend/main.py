bedf161cf9c09e50626814c7ff752a96cc2cbe4b
Jay <allmail@posteo.de>
2025-05-31 13:37:10 +0200

    Login, Registrierung und Aufforderung dazu eingebettet OHNE Datenbank.

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,47 @@
 #Einstiegspunkt
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
+from flask import g
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24) # Sessions für zb. Login
 
 @app.route("/")
 def index():
-    return render_template("index.html") #Startseite
+    user = session.get("user")
+    return render_template("index.html", user=user) #Startseite
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         #Hier später Login Logik
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # Beispielwerte
+        if email == "test@event.de" and password == "1234":
+            session["user"] = email
+            flash("Login erfolgreich")
+            return redirect(url_for("index"))
+        else:
+            flash("Login fehlgeschlagen")
+
         return redirect(url_for("index"))
     return render_template("login.html")
 
 @app.route("/register", methods=["GET","POST"])
-def regiser():
+def register():
     if request.method == "POST":
         # Hier später Registrierung
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # Hier validieren oder speichern
+        flash(f"Registrierung erfolgreich für {name} ({email})")
         return redirect(url_for("login"))
     return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
